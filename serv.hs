677f2f82911be26d01fc36990df3566ef0501524
Andrew Romanovsky <rmansorokin@gmail.com>
2018-03-08 21:33:17 +0300

    simple parsing and calculating request expression added

--- serv.hs
+++ serv.hs
@@ -1,16 +1,36 @@
-import Network.WebSockets
+import Prelude hiding (span)
+import Network.WebSockets --cabal install network, cabal install websockets
 import System.IO
-import qualified Data.ByteString as BS hiding (pack)
-import Data.ByteString.Char8 (pack)
+import qualified Data.ByteString.Char8 as BSC
+import Data.Word (Word8)
+import Data.Char (ord, isDigit)
+import qualified Data.Map as M
+
+mult = fromIntegral $ ord '*' :: Word8
+minus = fromIntegral $ ord '-' :: Word8
+divide = fromIntegral $ ord '/' :: Word8
+plus = fromIntegral $ ord '+' :: Word8
+
+ops = M.fromList [('*', (*)), ('-', (\a b -> a - b)), ('/', div), ('+', (+))]
+
+calcExpr :: BSC.ByteString -> Maybe Int
+calcExpr s = do
+    let trimmed = BSC.dropWhile (not . isDigit) s
+    (x, rs) <- BSC.readInt trimmed
+    let op = BSC.head rs
+        afterOp = BSC.tail rs
+    o <- M.lookup op ops
+    (y, _) <- BSC.readInt afterOp
+    return $ x `o` y
+
 
 main = runServer "127.0.0.1" 8080 handleConnection
   where
     handleConnection pending = do --handleConnection :: PendingConnection -> IO ()
         connection <- acceptRequest pending --calcRequest :: PendingConnection -> IO Connection
         let calcRequest = do
-            commandMsg <- receiveDataMessage connection
-            case commandMsg of
-                _ -> do
-                    sendTextData connection (pack "Hi! Your request is received!")
-                    calcRequest
+            commandMsg <- receiveData connection
+            case calcExpr commandMsg of
+                (Just x) -> sendTextData connection $ BSC.pack $ show x
+            calcRequest
         calcRequest
